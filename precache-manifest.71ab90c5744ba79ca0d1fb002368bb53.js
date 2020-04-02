b4c07ab6ca59d62697f1d275317df7f6fe515132
sivanh <sivanh@liveperson.com>
2020-04-02 15:50:57 +0300

    Updates

---
+++ precache-manifest.71ab90c5744ba79ca0d1fb002368bb53.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dd858c61ca062ee0e6ba489db4987f9",
+    "url": "/my-minesweeper-app/index.html"
+  },
+  {
+    "revision": "d9100dbaac1fd6188041",
+    "url": "/my-minesweeper-app/static/css/main.27aaed13.chunk.css"
+  },
+  {
+    "revision": "51ab41350cba7f86a978",
+    "url": "/my-minesweeper-app/static/js/2.ab1d3f7c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-minesweeper-app/static/js/2.ab1d3f7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9100dbaac1fd6188041",
+    "url": "/my-minesweeper-app/static/js/main.b6647660.chunk.js"
+  },
+  {
+    "revision": "1674d3bfe54c5e7150f6",
+    "url": "/my-minesweeper-app/static/js/runtime-main.594837c4.js"
+  },
+  {
+    "revision": "4f2d7cfb349863e17157a8577b1583e1",
+    "url": "/my-minesweeper-app/static/media/flag.4f2d7cfb.svg"
+  },
+  {
+    "revision": "364fd97086b696399e66a6682bc3d8b3",
+    "url": "/my-minesweeper-app/static/media/mine.364fd970.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sivanharel1234/my-minesweeper-app/precache-manifest.3786935fec03ce56b7278c31fefeb846.js"
+  "/my-minesweeper-app/precache-manifest.71ab90c5744ba79ca0d1fb002368bb53.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sivanharel1234/my-minesweeper-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-minesweeper-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
